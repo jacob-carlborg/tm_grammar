ff20298fa6ee7064b6221624376e15529cb4e593
Jacob Carlborg <doob@me.com>
2015-09-05 21:52:15 +0200

    Add some attributes to pattern:
    * begin
    * end
    * match
    * content_name

--- lib/tm_grammar/node/pattern.rb
+++ lib/tm_grammar/node/pattern.rb
@@ -12,6 +12,42 @@ module TmGrammar
       # @return [String] the scope name
       attr_reader :name
 
+      # Gets/sets match for the pattern.
+      #
+      # Corresponds to the `match` key in the TextMate grammar syntax.
+      #
+      # @param match [String, Regexp] the match to set
+      #
+      # @return [String, Regexp] the set match, or `nil` if not set
+      attr_accessor :match
+
+      # Gets/sets begin match for the pattern.
+      #
+      # Corresponds to the `begin` key in the TextMate grammar syntax.
+      #
+      # @param match [String, Regexp] the begin match to set
+      #
+      # @return [String, Regexp] the set being match, or `nil` if not set
+      attr_accessor :begin
+
+      # Gets/sets end match for the pattern.
+      #
+      # Corresponds to the `end` key in the TextMate grammar syntax.
+      #
+      # @param match [String, Regexp] the end match to set
+      #
+      # @return [String, Regexp] the set end match, or `nil` if not set
+      attr_accessor :end
+
+      # Gets/sets content name for the pattern.
+      #
+      # Corresponds to the `contentName` key in the TextMate grammar syntax.
+      #
+      # @param content_name [String, Regexp] the content name to set
+      #
+      # @return [String, Regexp] the set content name, or `nil` if not set
+      attr_accessor :content_name
+
       def initialize(name)
         @name = name
       end
--- spec/node/pattern_spec.rb
+++ spec/node/pattern_spec.rb
@@ -17,4 +17,40 @@ describe TmGrammar::Node::Pattern do
       grammar.name.should == name
     end
   end
+
+  describe 'match' do
+    let(:match) { /foo/ }
+
+    it 'gets/sets the match of the pattern' do
+      grammar.match = match
+      grammar.match.should == match
+    end
+  end
+
+  describe 'begin' do
+    let(:match) { /foo/ }
+
+    it 'gets/sets the being match of the pattern' do
+      grammar.begin = match
+      grammar.begin.should == match
+    end
+  end
+
+  describe 'end' do
+    let(:match) { /foo/ }
+
+    it 'gets/sets the end match of the pattern' do
+      grammar.end = match
+      grammar.end.should == match
+    end
+  end
+
+  describe 'content_name' do
+    let(:name) { 'foo' }
+
+    it 'gets/sets the content name of the pattern' do
+      grammar.content_name = name
+      grammar.content_name.should == name
+    end
+  end
 end
