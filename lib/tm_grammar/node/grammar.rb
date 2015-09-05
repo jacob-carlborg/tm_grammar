661ed1b8bebbe711cc13a676227b2acc5d0f5109
Jacob Carlborg <doob@me.com>
2015-09-05 17:39:20 +0200

    Add some TextMate grammar attributes:
    * file_types
    * folding_start_marker
    * folding_stop_marker
    * first_line_match

--- lib/tm_grammar/node/grammar.rb
+++ lib/tm_grammar/node/grammar.rb
@@ -11,6 +11,42 @@ module TmGrammar
       # @return [String] the scope name
       attr_reader :scope_name
 
+      # Gets/sets the file types of the grammar.
+      #
+      # Corresponds to `fileTypes` key in the TextMate grammar syntax.
+      #
+      # @param file_types [Array<String>] the file types to set
+      #
+      # @return [Array<String>] the file types
+      attr_accessor :file_types
+
+      # Gets/sets the file folding start marker of the grammar.
+      #
+      # Corresponds to `foldingStartMarker` key in the TextMate grammar syntax.
+      #
+      # @param marker [String, Regexp] the marker to set
+      #
+      # @return [String, Regexp, nil] the marker, or `nil` if none is set
+      attr_accessor :folding_start_marker
+
+      # Gets/sets the file folding stop marker of the grammar.
+      #
+      # Corresponds to `foldingStopMarker` key in the TextMate grammar syntax.
+      #
+      # @param marker [String, Regexp] the marker to set
+      #
+      # @return [String, Regexp, nil] the marker, or `nil` if none is set
+      attr_accessor :folding_stop_marker
+
+      # Gets/sets the first line match of the grammar.
+      #
+      # Corresponds to `firstLineMatch` key in the TextMate grammar syntax.
+      #
+      # @param match [String, Regexp] the regular expression to set
+      #
+      # @return [String, Regexp, nil] the match, or `nil` if none is set
+      attr_accessor :first_line_match
+
       # Initializes the receiver with the given scope name.
       #
       # @param scope_name [String] the scope name of the grammar.
--- spec/node/grammar_spec.rb
+++ spec/node/grammar_spec.rb
@@ -16,4 +16,40 @@ describe TmGrammar::Node::Grammar do
       grammar.scope_name.should == scope_name
     end
   end
+
+  describe 'file_types' do
+    let(:file_types) { %w(foo bar) }
+
+    it 'gets/sets the file types of the grammar' do
+      grammar.file_types = file_types
+      grammar.file_types.should == file_types
+    end
+  end
+
+  describe 'folding_start_marker' do
+    let(:marker) { /\{\s*$/ }
+
+    it 'gets/sets the folding start marker of the grammar' do
+      grammar.folding_start_marker = marker
+      grammar.folding_start_marker.should == marker
+    end
+  end
+
+  describe 'folding_stop_marker' do
+    let(:marker) { /^\s*\}/ }
+
+    it 'gets/sets the folding stop marker of the grammar' do
+      grammar.folding_stop_marker = marker
+      grammar.folding_stop_marker.should == marker
+    end
+  end
+
+  describe 'first_line_match' do
+    let(:match) { %r{#!\/bin\/bash} }
+
+    it 'gets/sets the folding stop marker of the grammar' do
+      grammar.first_line_match = match
+      grammar.first_line_match.should == match
+    end
+  end
 end
